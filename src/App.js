581f313728e885380dc5926eb633c326396e622d
rajus1 <javaninja@outlook.com>
2021-08-14 22:10:21 -0500

    adding react source

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BookComponent from './components/BookComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <BookComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/BookService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BOOKS_REST_API_URL = "http://localhost:8080/api/books";
+
+class BookService {
+
+    getBooks() {
+        return axios.get(BOOKS_REST_API_URL);
+    }
+}
+
+export default new BookService()
